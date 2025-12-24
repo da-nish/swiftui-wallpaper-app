4f997dc68df11c263352497cb7a05cfb0d66481b
danish khan <danishdev64@gmail.com>
2025-12-24 22:54:38 +0530

    Initial Commit

---
+++ wallpaper_app/wallpaper_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  wallpaper_appApp.swift
+//  wallpaper_app
+//
+//  Created by PropertyShare on 24/12/25.
+//
+
+import SwiftUI
+
+@main
+struct wallpaper_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
